f6b2041d9f31fca420ead6528b336c18d368f602
Karim <kizpucci@gmail.com>
2023-01-17 18:06:16 +0100

    [add]-route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboarCcontroller;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,13 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])
+    ->name('admin.karim')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/',[DashboarCcontroller::class, 'index'])->name('dashboard');
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
