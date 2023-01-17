a170e8af22ee884d1b824949325ed6b3dc754849
Karim <kizpucci@gmail.com>
2023-01-17 19:00:47 +0100

    [add]

--- app/Http/Controllers/Admin/DashboarCcontroller.php
+++ app/Http/Controllers/Admin/DashboarCcontroller.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class DashboarCcontroller extends Controller
 {
     public function index(){
-        return view('dashboard');
+        return view('admin.home');
     }
 }
